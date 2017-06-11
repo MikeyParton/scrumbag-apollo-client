2e888b530780ad992647f0f6dd6a1194b353c466
Mike Parton <mikeparton@live.com.au>
2017-06-11 17:17:19 -0400

    ADD: stateless components for Board, List & Card

---
+++ src/components/Board/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import List from '../List'
+
+export default props => (
+  <div>
+    <List />
+    <List />
+    <List />
+  </div>
+)
---
+++ src/components/List/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Card from '../Card'
+
+export default (props) => (
+  <div>
+    <Card title={'first card'} />
+    <Card title={'second card'} />
+  </div>
+)
